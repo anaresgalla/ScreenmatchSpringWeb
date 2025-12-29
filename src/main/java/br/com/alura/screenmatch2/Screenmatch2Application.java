2bf4f16e819af700b032384f256538725eec49b1
anaresgalla <anaresgalla@gmail.com>
2025-12-29 10:00:52 -0300

    Primeiros ajustes para transformar em spring web

--- src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
+++ src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -1,24 +1,12 @@
 package br.com.alura.screenmatch2;
 
-import br.com.alura.screenmatch2.main.Principal;
-import br.com.alura.screenmatch2.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Screenmatch2Application implements CommandLineRunner {
-	@Autowired
-	private SerieRepository repositorio;
+public class Screenmatch2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Screenmatch2Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
 	}
 }
