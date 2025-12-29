8655a53f7f52684146165b028e6397b5d86b66db
anaresgalla <anaresgalla@gmail.com>
2025-12-29 17:39:57 -0300

    Adiciona Devtools para live reload

--- src/main/java/br/com/alura/screenmatch2/config/CorsConfiguration.java
+++ src/main/java/br/com/alura/screenmatch2/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://localhost:5500/")
                 .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "HEAD",
                         "TRACE", "CONNECT");
     }
