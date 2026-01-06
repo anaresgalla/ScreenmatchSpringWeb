147f61328a6c4a7eb7ff6bb740ee0590a6a01c4d
anaresgalla <anaresgalla@gmail.com>
2026-01-06 16:59:15 -0300

    Exibe detalhes da série

--- src/main/java/br/com/alura/screenmatch2/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch2/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,5 +35,17 @@ public class SerieService {
                         s.getGenero(), s.getSinopse(), s.getAno(),
                         s.getElenco(), s.getPoster()))
                 .collect(Collectors.toList());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+        if(serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(),
+                    s.getTotalTemporadas(), s.getAvaliacao(),
+                    s.getGenero(), s.getSinopse(), s.getAno(),
+                    s.getElenco(), s.getPoster());
+        }
+        return null;
     }
 }
